afef441873fa087a1d339848d9c41e0a90e9d412
Mykhailo Sokolov <mcfordmiwok@gmail.com>
2025-03-24 20:59:23 +0200

    Second Commit

--- python/n1460_make_two_arrays_equal_by_reversing_subarrays.py
+++ python/n1460_make_two_arrays_equal_by_reversing_subarrays.py
@@ -3,7 +3,18 @@ from typing import List
 
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        pass
+        if len(target) != len(arr):
+            return False
+
+        target_counts = {}
+        for num in target:
+            target_counts[num] = target_counts.get(num, 0) + 1
+
+        arr_counts = {}
+        for num in arr:
+            arr_counts[num] = arr_counts.get(num, 0) + 1
+
+        return target_counts == arr_counts
 
 if __name__ == "__main__":
     target, arr = [1,2,3,4], [2,4,1,3]
